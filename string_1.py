9ca7d68e58738ebaa28ac1b7624c023690920efa
latikadhami05 <latikadhami06@gmail.com>
2025-07-22 21:26:46 +0530

    Strings

---
+++ string_1.py
@@ -0,0 +1,31 @@
+#now to print these strings
+name = "latika"
+age = 23
+price = 2.2
+
+print("name")   #only the word name is gonna be print not latika
+print(name) #now it will be print
+print(price)
+ 
+print("my name is:", name)
+
+age2=age
+print(age)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+#making boolean and none value
+age = 20
+old = False # cant write small f for false and true
+new = None
+print(type(old))
+print(type(new))
+
+a=2
+b=4
+sum=a+b
+diff=a-b
+print(sum)
+print(diff)
