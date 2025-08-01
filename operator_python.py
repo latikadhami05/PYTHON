9e65c137fc7bf0da84c02dbe2c36e6f80d7fef39
latikadhami05 <latikadhami06@gmail.com>
2025-08-01 21:42:53 +0530

    Types of Operator

---
+++ operator_python.py
@@ -0,0 +1,35 @@
+#arithmatic operator
+a=2
+b=5
+
+#print(a%b) #remainder
+#print(a**b) #for a^b
+
+
+#relational operator # for true and false
+a=50
+b=20
+print(a==b) #False
+print(a!=b) #True
+print(a>=b) #True kyuki 50 sai badha is 20
+print(a>b) #true
+
+
+#assignment operator
+num=10
+  # num=num+10 means #10+10 =>20 (this statemnet is same as num += 10)
+num *= 5 # 10 ko 5 sau multiply karega 
+
+print("num:",num)
+
+#logical operator
+v=5
+h=4
+print(not False)
+print(not (v>h))
+
+val1 = True
+val2 = False
+print("AND operator:", val1 and val2) # its T and ( operator) F thats is = false
+print("OR operator:", val1 or val2) # T or F = T
+print("OR operator:", (v==h)or(v>h)) # here 5 is not equalt to 4 so first false then as 5>4 so true and false or true is true
