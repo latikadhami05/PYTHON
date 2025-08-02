8cef5cbdc24f69f5079f5cbd46bfcc3b6f49c3ce
latikadhami05 <latikadhami06@gmail.com>
2025-08-02 10:16:31 +0530

    Type Conversion Casting

---
+++ typeConversionCasting.py
@@ -0,0 +1,21 @@
+a=2 #int
+b=4.25 #float
+
+sum=  a+b # int ko superior value that is float mai convert karega
+# that is 2.0+4.25 hogaa
+print(sum)
+# if - a="2" than cnat to a+b coz string mai add nhi hota - it will give error
+
+
+#type casting
+# uss a="2" ko ham string sai int mai badal saktee isme
+a=int("2")
+b=4.25
+#print(type(a))
+print(a+b)
+
+c=3.14
+c=str(c)
+print(type(c))
+# to convert the float value into string
+
